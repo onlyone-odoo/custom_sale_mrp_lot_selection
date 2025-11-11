41fabe0f297f56f5a1db176020edbd6ac91dec6b
onlyone-odoo <matiasb@onlyone.com.ar>
2025-11-11 17:10:58 -0300

    fix mrp production2

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -1,3 +1,4 @@
+# Copyright 2025 Your Company <https://yourcompany.com>
 # License AGPL-3.0 or later[](https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -14,11 +15,20 @@ class MrpProduction(models.Model):
         help="Lot of the main component propagated from sale order line.",
     )
 
-    def _get_move_raw_values(self, bom_line, line_data):
+    def _get_move_raw_values(
+        self, product, product_uom_qty, product_uom, operation_id=False, bom_line=False
+    ):
         """Override to restrict raw move to the selected component lot."""
-        vals = super()._get_move_raw_values(bom_line, line_data)
+        vals = super()._get_move_raw_values(
+            product,
+            product_uom_qty,
+            product_uom,
+            operation_id=operation_id,
+            bom_line=bom_line,
+        )
         if (
             self.component_lot_id
+            and bom_line
             and bom_line.product_id == self.component_lot_id.product_id
         ):
             vals["restrict_lot_id"] = self.component_lot_id.id
